1bae46b91424b6b67f88ad9714f29d9ee22b772c
Colin Brooks <mcbrooks@pm.me>
2022-04-12 21:24:26 -0400

    completed pull function of current & future forecasts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,67 @@ var city = 'Fort Myers, US'
 var ftmyersID = '4155995'
 var lat = '26.6217'
 var lon = '-81.8406'
-// var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial'
-var queryURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey + '&units=imperial'
-
+var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial'
+var queryURL2 = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey + '&units=imperial'
+var dt = []
+var humidity = []
+var temp_day = []
+var uvi = []
+var weather_main = []
+var weather_desc = []
+var icon = []
+var wind_speed = []
 
 
 function init() {
-    getAPI()
+    getAPI1()
+    getAPI2()
+    populate()
 }
 
 // var requestUrl = 'https://api.binance.com/api/v3/klines?symbol=ETHBTC&interval=1d&startTime=1617249600000&endTime=1617854400000'
 
-function getAPI() {
+function getAPI1() {
     fetch(queryURL)
     .then(function (response) {
       return response.json()
     })
     .then(function (data) {
-        console.log(data)
-        var timeNow =  new Date(data.current.dt*1000).toLocaleDateString("en-US")
-        var date0 = new Date(data.daily[0].dt*1000).toLocaleDateString("en-US")
-        var date1 = new Date(data.daily[1].dt*1000).toLocaleDateString("en-US")
-        var date2 = new Date(data.daily[2].dt*1000).toLocaleDateString("en-US")
-        var date3 = new Date(data.daily[3].dt*1000).toLocaleDateString("en-US")
-        var date4 = new Date(data.daily[4].dt*1000).toLocaleDateString("en-US")
-        var date5 = new Date(data.daily[5].dt*1000).toLocaleDateString("en-US")
-        console.log(timeNow, date0, date1, date2, date3, date4, date5)
+        // console.log(data)
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+    })    
+}
+
+function getAPI2() {
+    fetch(queryURL2)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data) {
+        // console.log(data)
+        dataArr = data.daily
+        for (var i = 0; i < dataArr.length; i++) {
+            console.log(dataArr[i])
+            dt[i] = new Date(dataArr[i].dt*1000).toLocaleDateString('en-US')
+            humidity[i] = dataArr[i].humidity
+            temp_day[i] = dataArr[i].temp.day
+            uvi[i] = dataArr[i].uvi
+            weather_main[i] = dataArr[i].weather[0].main
+            weather_desc[i] = dataArr[i].weather[0].description
+            icon[i] = dataArr[i].weather[0].icon
+            wind_speed[i] = dataArr[i].wind_speed
+        }
     })
 }
+
+function populate() {
+    for (var i = 0; i < dt.length; i++) {
+        
+    }
+}
+
+
 
 
 init()

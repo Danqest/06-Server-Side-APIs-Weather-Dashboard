7ff2bc336be9160cf7531b2733355630e5a7ac9d
Colin Brooks <mcbrooks@pm.me>
2022-04-14 17:49:03 -0400

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ var city = ""
 var APIKey = 'e2e8aa238c17cef17b8835fdca0cfb44'
 var storedSearches = []
 
+// function to initialize on page load/refresh
 function init() {
     initloadBtns()
 }
 
+// // run local storage search if any, populate with historical searched items
 function initloadBtns() {
     storedSearches = JSON.parse(localStorage.getItem('successful-searches'))
     if (storedSearches != null) {
@@ -34,13 +36,15 @@ function initloadBtns() {
     }
 }
 
+// populate with one search item upon successful search as a button 
 function loadBtns(city) {
     // check if value already exists in local storage
     // if not, add to local storage and populate button
     storedSearches = JSON.parse(localStorage.getItem('successful-searches'))
     
+    // if no stored search history
     if (storedSearches === null) {
-        // add to local storage, erase prior buttons, and populate all buttons
+        // populate first new search history city
         var histBtn = document.createElement('btn')
         histBtn.classList = 'btn btn-outline-success me-2'
         histBtn.setAttribute('type', 'button')
@@ -55,6 +59,7 @@ function loadBtns(city) {
         })
         storedSearches = []
         storedSearches[0] = city
+        // save historical search to local storage
         localStorage.setItem('successful-searches', JSON.stringify(storedSearches))
     }
     else if (storedSearches != null) {
@@ -80,12 +85,14 @@ function loadBtns(city) {
     }
 }
 
+// api call to confirm city name, lat, and lon which will be used for forecasted data
 function getAPI(city) {
     var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial'
     fetch(queryURL)
     .then(function (response) {
       return response.json()
     })
+    // if returned json is not 404, search forecasted data
     .then(function (data) {
         if (data.cod != '404') {
             city = data.name
@@ -96,8 +103,11 @@ function getAPI(city) {
             .then(function (response){
                 return response.json()
             })
+            // populate first content container with current & forecasted weather conditions
             .then(function (data) {
                 dataArr = data.daily
+
+                // for current conditions to be prominently displayed
                 for (var i = 0; i < 1; i++) {          
 
                     var cardEl = document.createElement('div')
@@ -106,11 +116,13 @@ function getAPI(city) {
                     var cardBody = document.createElement('div')
                     cardBody.classList = 'card-body'
 
+                    // date
                     var cardTitle = document.createElement('h2')
                     cardTitle.classList = 'card-title'
                     cardTitle.innerHTML = new Date(dataArr[i].dt*1000).toLocaleDateString('en-US')
                     cardBody.appendChild(cardTitle)
 
+                    // img representing weather
                     var img = document.createElement('img')
                     img.classList = "img-fluid rounded-start"
                     img.setAttribute('src', ('./assets/images/'+dataArr[i].weather[0].main+'.png'))
@@ -121,14 +133,17 @@ function getAPI(city) {
                     cardSubtitle.innerHTML = city
                     cardBody.appendChild(cardSubtitle)
 
+                    // humidity
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Humidity: ' + dataArr[i].humidity
                     cardBody.appendChild(weatherLi)
 
+                    // temp
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Temperature: ' + dataArr[i].temp.day
                     cardBody.appendChild(weatherLi)
 
+                    // color coded uv index
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'UV-Index: ' + dataArr[i].uvi
                     if (dataArr[i].uvi < 3) {
@@ -148,23 +163,27 @@ function getAPI(city) {
                     }
                     cardBody.appendChild(weatherLi)
 
+                    // wind speed
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Wind Speed: ' + dataArr[i].wind_speed
                     cardBody.appendChild(weatherLi)
 
+                    // current weather and qualifying description
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Weather: ' + dataArr[i].weather[0].main + " - " + dataArr[i].weather[0].description
                     cardBody.appendChild(weatherLi)
                     
-                    
+                    // add to container
                     cardEl.appendChild(cardBody)
                     containerEl.appendChild(cardEl)
                 }
 
+                // create group container for forecast to be displayed in
                 var cardGroup = document.createElement('div')
                 cardGroup.classList = 'card-group'
                 container2El.appendChild(cardGroup)
 
+                // for 5-day future forecasted conditions
                 for (var i = 1; i < 6; i++) {
 
                     var cardEl = document.createElement('div')
@@ -173,24 +192,29 @@ function getAPI(city) {
                     var cardBody = document.createElement('div')
                     cardBody.classList = 'card-body'
 
+                    // date
                     var cardTitle = document.createElement('h2')
                     cardTitle.classList = 'card-title'
                     cardTitle.innerHTML = new Date(dataArr[i].dt*1000).toLocaleDateString('en-US')
                     cardBody.appendChild(cardTitle)
 
+                    // weather emoji
                     var img = document.createElement('img')
                     img.classList = "img-fluid rounded-start"
                     img.setAttribute('src', ('./assets/images/'+dataArr[i].weather[0].main+'.png'))
                     cardBody.appendChild(img)
 
+                    // humidity
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Humidity: ' + dataArr[i].humidity
                     cardBody.appendChild(weatherLi)
 
+                    // temp
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Temperature: ' + dataArr[i].temp.day
                     cardBody.appendChild(weatherLi)
 
+                    // color coded UV index
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'UV-Index: ' + dataArr[i].uvi
                     if (dataArr[i].uvi < 3) {
@@ -210,10 +234,12 @@ function getAPI(city) {
                     }
                     cardBody.appendChild(weatherLi)
 
+                    // wind speed
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Wind Speed: ' + dataArr[i].wind_speed
                     cardBody.appendChild(weatherLi)
 
+                    // weather & description
                     var weatherLi = document.createElement('p')
                     weatherLi.innerHTML = 'Weather: ' + dataArr[i].weather[0].main + " - " + dataArr[i].weather[0].description
                     cardBody.appendChild(weatherLi)
@@ -225,14 +251,17 @@ function getAPI(city) {
                 subtitle.innerHTML = 'Current Conditions:'
                 subtitle2.innerHTML = '5-Day Forecast for ' + city
 
+                // create search history buttons
                 loadBtns(city)
             })
+            // if 404, return Not Found
         } else alert(city + " not found! Please try a valid location.")
         
     })   
     
 }
 
+// clear containers function
 function clearForm() {
     const cardEl = document.querySelector('.card')
     if (cardEl != undefined) {
@@ -245,7 +274,7 @@ function clearForm() {
     
 }
 
-
+// clear containers on search with searchbox
 function searchCityBox() {
     event.preventDefault();
     var city = searchTerm.value.trim()
@@ -257,7 +286,8 @@ function searchCityBox() {
         alert('Please enter a location.')
     }
 }
-
+// search button
 formEl.addEventListener('submit', searchCityBox)
 
+// execute on load
 init()

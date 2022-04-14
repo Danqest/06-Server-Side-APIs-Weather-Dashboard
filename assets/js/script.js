ce140f6d2e9ced3faecf2febb80472b40acc56c1
Colin Brooks <mcbrooks@pm.me>
2022-04-13 22:16:35 -0400

    added basic formatting for current & forecast display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-var cardContainerEl = document.querySelector('#cards-container')
+// var rowEl = document.querySelector('.row')
+var containerEl = document.querySelector('.container')
+var container2El = document.querySelector('.container2')
+var cardGroup = document.querySelector('.card-group')
+var subtitle2 = document.querySelector('.subtitle2')
 
 var APIKey = 'e2e8aa238c17cef17b8835fdca0cfb44'
 var city = 'Fort Myers, US'
@@ -34,7 +38,7 @@ function getAPI2() {
     .then(function (data) {
         // console.log(data)
         dataArr = data.daily
-        for (var i = 0; i < 6; i++) {
+        for (var i = 0; i < 1; i++) {
             // console.log(dataArr[i])
             // dt[i] = new Date(dataArr[i].dt*1000).toLocaleDateString('en-US')
             // humidity[i] = dataArr[i].humidity
@@ -44,39 +48,94 @@ function getAPI2() {
             // weather_desc[i] = dataArr[i].weather[0].description
             // icon[i] = dataArr[i].weather[0].icon
             // wind_speed[i] = dataArr[i].wind_speed
+            
 
             var cardEl = document.createElement('div')
-            cardEl.classList = 'list-item flex-row justify-space-between align-center'
+            cardEl.classList = 'card text-center'
 
-            var weatherEl = document.createElement('ul')
-            weatherEl.classList = 'flex-row align-center';
-            weatherEl.innerHTML = new Date(dataArr[i].dt*1000).toLocaleDateString('en-US')
-            
-            var weatherLi = document.createElement('li')
+            var cardBody = document.createElement('div')
+            cardBody.classList = 'card-body'
+
+            var cardTitle = document.createElement('h2')
+            cardTitle.classList = 'card-title'
+            cardTitle.innerHTML = new Date(dataArr[i].dt*1000).toLocaleDateString('en-US')
+            cardBody.appendChild(cardTitle)
+
+            var cardSubtitle = document.createElement('h3')
+            cardSubtitle.classList = 'card-subtitle mb-2 text-muted'
+            cardSubtitle.innerHTML = city
+            cardBody.appendChild(cardSubtitle)
+
+            var weatherLi = document.createElement('p')
             weatherLi.innerHTML = 'Humidity: ' + dataArr[i].humidity
-            weatherEl.appendChild(weatherLi)
+            cardBody.appendChild(weatherLi)
 
-            var weatherLi = document.createElement('li')
+            var weatherLi = document.createElement('p')
             weatherLi.innerHTML = 'Temperature: ' + dataArr[i].temp.day
-            weatherEl.appendChild(weatherLi)
+            cardBody.appendChild(weatherLi)
 
-            var weatherLi = document.createElement('li')
+            var weatherLi = document.createElement('p')
             weatherLi.innerHTML = 'UV-Index: ' + dataArr[i].uvi
-            weatherEl.appendChild(weatherLi)
+            cardBody.appendChild(weatherLi)
 
-            var weatherLi = document.createElement('li')
+            var weatherLi = document.createElement('p')
             weatherLi.innerHTML = 'Wind Speed: ' + dataArr[i].wind_speed
-            weatherEl.appendChild(weatherLi)
+            cardBody.appendChild(weatherLi)
 
-            var weatherLi = document.createElement('li')
+            var weatherLi = document.createElement('p')
             weatherLi.innerHTML = 'Weather: ' + dataArr[i].weather[0].main + " - " + dataArr[i].weather[0].description
-            weatherEl.appendChild(weatherLi)
+            cardBody.appendChild(weatherLi)
             
             
             // weatherEl.appendChild(weatherLi)
-            cardEl.appendChild(weatherEl)
-            cardContainerEl.appendChild(cardEl)
+            cardEl.appendChild(cardBody)
+            containerEl.appendChild(cardEl)
         }
+        for (var i = 1; i < 6; i++) {
+
+            var cardEl = document.createElement('div')
+            cardEl.classList = 'card'
+
+            var cardBody = document.createElement('div')
+            cardBody.classList = 'card-body'
+
+            var cardTitle = document.createElement('h2')
+            cardTitle.classList = 'card-title'
+            cardTitle.innerHTML = new Date(dataArr[i].dt*1000).toLocaleDateString('en-US')
+            cardBody.appendChild(cardTitle)
+
+            // var cardSubtitle = document.createElement('h3')
+            // cardSubtitle.classList = 'card-subtitle mb-2 text-muted'
+            // cardSubtitle.innerHTML = city
+            // cardBody.appendChild(cardSubtitle)
+
+            var weatherLi = document.createElement('p')
+            weatherLi.innerHTML = 'Humidity: ' + dataArr[i].humidity
+            cardBody.appendChild(weatherLi)
+
+            var weatherLi = document.createElement('p')
+            weatherLi.innerHTML = 'Temperature: ' + dataArr[i].temp.day
+            cardBody.appendChild(weatherLi)
+
+            var weatherLi = document.createElement('p')
+            weatherLi.innerHTML = 'UV-Index: ' + dataArr[i].uvi
+            cardBody.appendChild(weatherLi)
+
+            var weatherLi = document.createElement('p')
+            weatherLi.innerHTML = 'Wind Speed: ' + dataArr[i].wind_speed
+            cardBody.appendChild(weatherLi)
+
+            var weatherLi = document.createElement('p')
+            weatherLi.innerHTML = 'Weather: ' + dataArr[i].weather[0].main + " - " + dataArr[i].weather[0].description
+            cardBody.appendChild(weatherLi)
+            
+            
+            // weatherEl.appendChild(weatherLi)
+            cardEl.appendChild(cardBody)
+            cardGroup.appendChild(cardEl)
+            // container2El.appendChild(cardGroup)
+        }
+        subtitle2.innerHTML = '5-Day Forecast for ' + city
     })
 }
 
